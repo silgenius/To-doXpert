472c4448cf60a59ff99040ffbff28ab4d49f6f6b
silgenius <olutademartin@gmail.com>
2023-10-24 02:31:41 -0700

    file to print main menu message

---
+++ print_prompt.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void print_prompts(void)
+{
+	printf("WELCOME to the To-doXpert program\n");
+	printf("1. Add a task\n");
+	printf("2. Remove a task\n");
+	printf("3. View task(s)\n");
+	printf("4. Mark a task as completed\n");
+	printf("5. Sort tasks\n");
+	printf("6. Filter tasks\n");
+	printf("7. Save tasks to file\n");
+	printf("8. Load tasks from file\n");
+	printf("9. Quit\n");
+}
