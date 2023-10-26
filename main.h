c4a7d4813b2e474f2c735522f16daa3c03d70938
silgenius <olutademartin@gmail.com>
2023-10-26 15:52:59 -0700

    update header file

--- main.h
+++ main.h
@@ -3,21 +3,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void print_prompts(void);
+void add_task(void);
+void delete_task();
+size_t read_line(char *str, size_t size);
+void task_to_file(char *filename);
+void print_task(void);
+void mark_as_completed(void);
 
 typedef struct task_s
 {
+	int index;
 	char *name;
-	char status[11];
-	task_s *next
+	char *status;
+	char *due_date;
+	struct task_s *next;
 }task;
 
-#ifdef TASK
-	task *head = NULL;
-#else
-	extern task *head;
-#endif /* TASK */
+extern task *head;
+extern int task_index;
 
+int str_to_int(char *input);
+void task_loop(void);
+int Choice2Task(int choice);
+void free_mem(void);
 
+char *index_to_name(int index);
 #endif /* MAIN_H */
